8c9086fb68870396670e931bfabf81969a7c3380
Jonathan Baggott <jonathan.baggott82@gmail.com>
2022-06-26 19:17:41 +0100

    Add project files.

---
+++ Palindrome/Models/Palindrome.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PalindromeMVCApp.Models
+{
+    public class Palindrome
+    {
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string InputWord { get; set; }
+
+        public string RevWord { get; set; }
+        public bool IsPalindrome { get; set; }
+        public string Message { get; set; }
+    }
+}
